36b5dbc58c60b06622010fec0d8bbae80a755a5f
ignat <ignatik.klokov@gmail.com>
2025-01-11 12:15:16 +0200

    add gallery support

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def download(url, name):
+  urllib.request.urlretrieve(url, name)
+
+def downloadGallery(post:praw.Reddit.submission, name):
+  # watch this break within 2-3 months
+  media_id = post.gallery_data['items'][0]["media_id"]
+  url = i.media_metadata[media_id]["s"]["u"]
+  download(url, name)
+
 SIZE = 2048
 WIDTH = 8
 HEIGHT = 8
@@ -22,9 +31,6 @@ if (input("download top images? (y/N): ") == "y"):
     user_agent="python:birdification:v1.0 (by u/johncraft2003)"
   )
 
-  def download(url, name):
-    urllib.request.urlretrieve(url, name)
-
   submissions = reddit.subreddit("birdification").top(limit=WIDTH * HEIGHT, time_filter="month")
   s = False
   for k, i in enumerate(submissions):
@@ -32,6 +38,9 @@ if (input("download top images? (y/N): ") == "y"):
     if (re.match("https://i\.redd\.it/[a-zA-Z0-9]+\.(jpeg|png)", i.url)):
       # print("valid")
       download(i.url, f"images/{k}.jpeg") # assume jpeg since its the majority 
+    elif (re.match("https://www\.reddit\.com/gallery/[a-zA-Z0-9]+", i.url)): 
+      print(f"found a gallery: {i.title} ({i.url}). downloading the first photo")
+      downloadGallery(i, f"images/{k}.jpeg") # gallery things seem to return webp 
     else:
       print(f"found controversial post: {i.title} ({i.url}). ignoring for now")
       s = True
@@ -48,7 +57,7 @@ imgs = [os.path.join("images", f) for f in os.listdir("images")
         if os.path.isfile(os.path.join("images", f)) and f.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
 if not imgs:
-  print("confusion!!!!!!!!!!! no images????")
+  print("CONFUSION!!!!!!!!!!! no images????")
   exit()
 
 # assuming tiles are squared
